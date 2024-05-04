7c5ffff8ba0b8d02fd53f7dce828088527c8b73d
JorgeMejiaH <j.mejia2@utp.edu.co>
2024-05-04 18:32:01 -0500

    nueva API CRUD

---
+++ CRUD.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+import certifi
+import uuid
+
+app = Flask(__name__)
+
+MONGO_URI  = 'mongodb+srv://jMejia2:fCn5fSLzmA07nh3H@cluster0.p0kwqwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+client =MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+db = client.get_database("miInventario")
+collection = db.productos
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Obtener todos los productos
+@app.route('/productos', methods=['GET'])
+def get_productos():
+    productos = list(collection.find({}, {'_id': 0}))
+    return jsonify(productos)
+
+# Obtener un producto por su ID
+@app.route('/productos/<string:id>', methods=['GET'])
+def get_producto(id):
+    producto = collection.find_one({'id': id}, {'_id': 0})
+    if producto:
+        return jsonify(producto)
+    return jsonify({"error": "Producto no encontrado"}), 404
+
+# Crear un nuevo producto
+@app.route('/productos', methods=['POST'])
+def crear_producto():
+    nuevo_producto = request.get_json()
+    nuevo_producto['id'] = str(uuid.uuid4())
+    resultado = collection.insert_one(nuevo_producto)
+    return jsonify(nuevo_producto), 201
+
+# Actualizar un producto
+@app.route('/productos/<string:id>', methods=['PUT'])
+def actualizar_producto(id):
+    producto_data = request.get_json()
+    resultado = collection.update_one({'id': id}, {'$set': producto_data})
+    if resultado.modified_count > 0:
+        return jsonify(producto_data)
+    return jsonify({"error": "Producto no encontrado"}), 404
+
+# Eliminar un producto
+@app.route('/productos/<string:id>', methods=['DELETE'])
+def eliminar_producto(id):
+    resultado = collection.delete_one({'id': id})
+    if resultado.deleted_count > 0:
+        return jsonify({"mensaje": "Producto eliminado"})
+    return jsonify({"error": "Producto no encontrado"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
